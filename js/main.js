b1d81514a0a22c6c6be30ac02f18c042ac615a4d
pipetabionica <86312185+pipetabionica@users.noreply.github.com>
2021-09-06 23:45:52 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,46 +1,39 @@
-//const camperaCanelon = {
-//    name: "Camperas Benihana",
-//    price: "14000$",
-//    stock: 10
-//}
-////console.log(camperaCanelon)
-//
-//
-////const----->guarda solo info que no se puede modificar, por lo que hay que utilizar un LET.
-//
-//let acumulador = ``;
-//for(let i = 0; i< 3; i++){
-//    acumulador+= `<div>
-//    <h3>${camperaCanelon.name}</h3>
-//    <b>${camperaCanelon.price}</b>
-//    <p>${camperaCanelon.stock}</p>
-//    </div>`
-//}
-//
-//document.write(acumulador);
+class Producto {
+    constructor (pagina, imagen, modelo, precio, stock, talle) {
+        this.pagina = pagina;
+        this.imagen = imagen;
+        this.modelo = modelo;
+        this.precio = precio;
+        this.stock = stock;
+        this.talle = talle;
+    }
+};
 
-//Funcion constructora - sirve para crear objetos
-//function Persona (nombre, edad, calle) {
-//    this.nombre = nombre;
-//    this.edad = edad;
-//    this.calle = calle;
-//}
-//const persona1 = new Persona ('Raul', 23 , roosevelt 231)
+const producto1=new Producto (`CampeGrisyAzul.html`, `image/Cuadros hover/campera gris hombre.jpg`, `Campera Rompevientos`, `1500$`, 203, `XL`);
+const producto2=new Producto (`CanelonAzul.html`, `image/Cuadros hover/canelon azul.jpg`, `Campera de abrigo`, `1650$`, 23, "L");
+const producto3=new Producto (`CanelonCeleste.html`, `image/Cuadros hover/canelon celeste.jpg`, `Campera de abrigo`, `1650$`, 23, `S, M, L`);
+const producto4=new Producto (`Canelongris.html`, `image/Cuadros hover/canelon gris.jpg`, `Campera de abrigo`, `1650$`, 2, `S, L, XL`);
+const producto5=new Producto (`ProGris.html`, `image/Cuadros hover/pro gris mujer.jpg`, `Campera de cerro`, `17500$`, 15, `L,XL,XXL`);
+const producto6=new Producto (`CanelonRojo.html`, `image/Cuadros hover/canelon rojo.jpg`, `Campera de abrigo`, `1650$`, 26, `S`);
 
-//-------------------------------------------------------------------------> es6
-// class Producto {
-//     constructor (nombre, precio, stock){
-//         this.nombre = nombre;
-//         this.precio = precio;
-//         this.stock = stock;
-//     }
-// 
-//     validarStock (cantidad){
-//         alert("tengo stock para vos" + cantidad)
-//     }
-// }
-// 
-// const producto = new Producto ("Campera Canelones", 15000, 7)
-// producto.validarStock(7);
+const baseDedatos = [producto1, producto2, producto3, producto4, producto5, producto6];
 
-const producto
+let productos=[]
+let acumuladorstock = ``;
+
+baseDedatos.forEach ((producto) => {
+    acumuladorstock += (`<div id="Cards" class="cartas col-lg-4 col-md-4 col-sm-5 col-xs-12">
+    <a href=${producto.pagina}><div class="c-img">
+        <img src="${producto.imagen}" alt="FotosProductos" class= "h-100">
+        <div class="txt">
+            <h3>${producto.modelo}</h3>
+            <h5>${producto.precio}</h5>
+            <h6>Quedan: ${producto.stock}</h6>
+            <button id="comprar">Agregar al carriito</button>
+        </div>
+    </div></a>
+    </div>`)
+
+});
+
+document.getElementById("Cards").innerHTML = acumuladorstock
