3b8d495552f3d295d7dc1d3980df85eb3f292153
Felipe <86312185+PipeMartini@users.noreply.github.com>
2021-09-15 19:15:50 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 class Producto {
-    constructor (pagina, imagen, modelo, precio, stock, talle) {
+    constructor (id, pagina, imagen, modelo, precio, stock, talle) {
+        this.id=id;
         this.pagina = pagina;
         this.imagen = imagen;
         this.modelo = modelo;
@@ -8,13 +9,13 @@ class Producto {
         this.talle = talle;
     }
 };
-
-const producto1=new Producto (`CampeGrisyAzul.html`, `image/Cuadros hover/campera gris hombre.jpg`, `Campera Rompevientos`, `1500$`, 203, `XL`);
-const producto2=new Producto (`CanelonAzul.html`, `image/Cuadros hover/canelon azul.jpg`, `Campera de abrigo`, `1650$`, 23, "L");
-const producto3=new Producto (`CanelonCeleste.html`, `image/Cuadros hover/canelon celeste.jpg`, `Campera de abrigo`, `1650$`, 23, `S, M, L`);
-const producto4=new Producto (`Canelongris.html`, `image/Cuadros hover/canelon gris.jpg`, `Campera de abrigo`, `1650$`, 2, `S, L, XL`);
-const producto5=new Producto (`ProGris.html`, `image/Cuadros hover/pro gris mujer.jpg`, `Campera de cerro`, `17500$`, 15, `L,XL,XXL`);
-const producto6=new Producto (`CanelonRojo.html`, `image/Cuadros hover/canelon rojo.jpg`, `Campera de abrigo`, `1650$`, 26, `S`);
+ 
+const producto1=new Producto (`1`,`CampeGrisyAzul.html`, `image/Cuadros hover/campera gris hombre.jpg`, `Campera Rompevientos`, `1500$`, 203, `XL`);
+const producto2=new Producto (`2`,`CanelonAzul.html`, `image/Cuadros hover/canelon azul.jpg`, `Campera de abrigo`, `1650$`, 23, "L");
+const producto3=new Producto (`3`,`CanelonCeleste.html`, `image/Cuadros hover/canelon celeste.jpg`, `Campera de abrigo`, `1650$`, 23, `S, M, L`);
+const producto4=new Producto (`4`,`Canelongris.html`, `image/Cuadros hover/canelon gris.jpg`, `Campera de abrigo`, `1650$`, 2, `S, L, XL`);
+const producto5=new Producto (`5`,`ProGris.html`, `image/Cuadros hover/pro gris mujer.jpg`, `Campera de cerro`, `17500$`, 15, `L,XL,XXL`);
+const producto6=new Producto (`6`,`CanelonRojo.html`, `image/Cuadros hover/canelon rojo.jpg`, `Campera de abrigo`, `1650$`, 26, `S`);
 
 const baseDedatos = [producto1, producto2, producto3, producto4, producto5, producto6];
 
@@ -29,11 +30,11 @@ baseDedatos.forEach ((producto) => {
             <h3>${producto.modelo}</h3>
             <h5>${producto.precio}</h5>
             <h6>Quedan: ${producto.stock}</h6>
-            <button id="comprar">Agregar al carriito</button>
         </div>
     </div></a>
+    <button id="comprar" onclick="AgregarAlCarro(${Producto.id})">Agregar al carriito</button>
     </div>`)
-
 });
 
-document.getElementById("Cards").innerHTML = acumuladorstock
+document.getElementById("Cards").innerHTML = acumuladorstock;
+
